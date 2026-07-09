e5e29dcb3ebc78a65421be2fb6c4fa3e30dd60a5
Chinmay K Shetty <ckshetty004@gmail.com>
2026-07-09 10:44:31 +0530

    Checking Pipeline3

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Hello from CKShetty GitOps PoC Testing")
+        self.wfile.write(b"This is Me testing B4 Presentation")
 
 HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
