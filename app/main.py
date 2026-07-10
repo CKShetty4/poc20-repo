e9ba6e9c6f54891007412a7b5abe5ec51ee200cf
Chinmay K Shetty <ckshetty004@gmail.com>
2026-07-10 17:12:47 +0530

    Checking Pipeline56

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"This is Me testing")
+        self.wfile.write(b"This is Me Chinmay K Shetty in Poc presentation")
 
 HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
